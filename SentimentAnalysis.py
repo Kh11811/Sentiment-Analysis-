ea3467fd4112877394ec1d800fe6a749269f0eb1
Kh11811 <khalilgabssi8@gmail.com>
2025-06-10 11:04:00 +0100

    Update SentimentAnalysis.py

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -9,16 +9,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
-#adding stars column (train data)
-file_path = '/content/drive/MyDrive/Colab datasets/SentimentAnalysis/train.csv'
-df_train = pd.read_csv(file_path)
-df_train = df_train.sample(frac=1).reset_index(drop=True)
-#adding stars column (test data)
-file_path = '/content/drive/MyDrive/Colab datasets/SentimentAnalysis/train.csv'
-df_test = pd.read_csv(file_path)
-df_test = df_test.sample(frac=1).reset_index(drop=True)
+
+file_path_train = 'train.csv'
+df_train = pd.read_csv(file_path_train)
+
+file_path_test = 'test.csv'
+df_test = pd.read_csv(file_path_test)
+
 df = pd.concat([df_train,df_test],axis=0)
 df['Sentiment'] = (df['Sentiment'] == 'pos').astype(int)
+
+df = df.sample(frac=1).reset_index(drop=True)
 #splitting data
 vectorizer = TfidfVectorizer()
 x_reviews = df['Review'].values
@@ -30,10 +31,10 @@ pipeline = Pipeline([
 ])
 pipeline.fit(x_train, y_train)
 y_pred = pipeline.predict(x_test)
-print('Accuracy:', accuracy_score(y_test, y_pred))
-print(classification_report(y_test, y_pred))
-import joblib
 
+print(classification_report(y_test, y_pred))
+
+import joblib
 # Save pipeline
 joblib.dump(pipeline, '/content/drive/MyDrive/Colab datasets/sentiment_pipeline.joblib')
 
